31a6adbab479e13143d680f9cbb65ec707eed80b
Bry Cole <Brysonc98@live.com>
2022-01-09 20:07:29 -0800

    added Portfolio page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,14 +3,13 @@ import React from 'react';
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>About Me</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      Hi, I'm Bryson Cole.
+      Thank you for visting my React Portfolio. I have been a junior devloper at Intuli LLc. for four years and a developer for the past year.
+      I am currently enrolled in the UW Full-Stack developer bootcamp to increase my skills. 
+      I have completed the two-thirds of the UW Bootcamp. I have made front end and back end apps thrtoughout the bootcamp.
+      This Portfolio will place some of the work I have completed during the bootcamp and eventually my own individual projects.
       </p>
     </div>
   );
--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -1,24 +1,33 @@
 import React from 'react';
+import '../styles/Portfolio.css';
 
 export default function Blog() {
   return (
     <div>
-      <h1>Blog Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+    <section id="Work-Section" class="Work-Section">
+      <div class="Work-Section div">
+        <a class="Work-Section-1" href="https://bcole37.github.io/Horiseon-Code-Refractor/"  title="Work-Section-1" id="Work-Section-1">
+            <div class="bottom-left">Code Refractor</div>
+            <a href="https://bcole37.github.io/Horiseon-Code-Refractor/"></a>
+        </a>
+        <a class="Work-Section-2" href="https://protected-falls-91774.herokuapp.com/"  title="Work-Section-2" id="Work-Section-2">
+        <div class="bottom-left-small">Music Trivia</div>
+        </a>
+        <a class="Work-Section-3" href="https://bcole37.github.io/Weather-Dashboard/"  title="Work-Section-3" id="Work-Section-3">
+            <div class="bottom-left-small">Weather-Dashboard</div>
+        </a>
+        <a class="Work-Section-4" href="https://bcole37.github.io/Work-Scheduler/"  title="Work-Section-4" id="Work-Section-4">
+            <div class="bottom-left-small">Work-Scheduler</div>
+        </a>
+        <a class="Work-Section-5" href="https://bcole37.github.io/Password-Creator/"  title="Work-Section-5" id="Work-Section-5">
+            <div class="bottom-left-small">Password-Creator</div>
+        </a>
+        <a class="Work-Section-6" href="https://intense-mountain-22968.herokuapp.com/"  title="Work-Section-6" id="Work-Section-6">
+            <div class="bottom-left-small">Note-Taker</div>
+        </a>
+      </div>
+        
+    </section>
     </div>
   );
 }
